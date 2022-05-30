dcf27b2e4c60beff946eee9531901c81ee895483
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-05-30 11:20:30 +0300

    Add data and add settings on the page.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import './App.css'
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { FiSettings } from "react-icons/fi";
 
 const App = () => {
   return (
-    <div className="underline text-3xl">App</div>
+    <div className="flex relative dark:bg-main-dark-bg">
+      <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+        <TooltipComponent content="Settings" position="Top">
+          <button
+            type="button"
+            className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+            style={{ backgroundColor: 'blue', borderRadius: '50%'}}
+          >
+            <FiSettings />
+          </button>
+        </TooltipComponent>
+      </div>
+    </div>
   )
 }
 export default App;
