c36eeac12a3c5fa225dc766741e93eeb40fbef05
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-06-08 00:15:04 +0300

    Add prettier and fix dependencies

---
+++ prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  printWidth: 80,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  trailingComma: "all",
+  bracketSpacing: true,
+  singleQuote: true,
+};
--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
-import React, {useState} from 'react';
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import { FiSettings } from "react-icons/fi";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   const [activeMenu, setActiveMenu] = useState(true);
   return (
-    <div className="flex relative dark:bg-main-dark-bg">
-      <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
-        <TooltipComponent content="Settings" position="Top">
-          <button
-            type="button"
-            className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
-            style={{ backgroundColor: 'blue', borderRadius: '50%'}}
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                style={{ backgroundColor: "blue", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              Sidebar
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">Closed</div>
+          )}
+          <div
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? "md:ml-72" : "flex-2"
+            }`}
           >
-            <FiSettings />
-          </button>
-        </TooltipComponent>
-      </div>
-      {activeMenu ? (
-        <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-          Sidebar
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark navbar w-full">
+              Navbar
+            </div>
+          </div>
         </div>
-      ) : (
-        <div className="w-0 dark:bg-secondary-dark-bg">
-          Closed
-        </div>
-      )}
-      <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
-        <div className="fixed md:static bg-main-bg dark:bg-main-dark navbar w-full">
-          Navbar
-        </div>
-      </div>
+      </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 export default App;
