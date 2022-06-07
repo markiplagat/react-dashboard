f2bbbc4f51d03f62839c4f0ceafd0801fb83a68a
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-06-07 23:00:31 +0300

    Adding sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css'
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import { FiSettings } from "react-icons/fi";
 
 const App = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
   return (
     <div className="flex relative dark:bg-main-dark-bg">
       <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
@@ -16,6 +17,20 @@ const App = () => {
             <FiSettings />
           </button>
         </TooltipComponent>
+      </div>
+      {activeMenu ? (
+        <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+          Sidebar
+        </div>
+      ) : (
+        <div className="w-0 dark:bg-secondary-dark-bg">
+          Closed
+        </div>
+      )}
+      <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
+        <div className="fixed md:static bg-main-bg dark:bg-main-dark navbar w-full">
+          Navbar
+        </div>
       </div>
     </div>
   )
