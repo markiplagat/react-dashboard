9198323d9d66486dcf47b3550894502009826848
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-06-14 00:25:53 +0300

    Find the screen size and display navbar and sidebar accordingly

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,7 +8,12 @@ import {links} from "../data/dummy";
 import {useStateContext} from "../context/ContextProvider";
 
 const Sidebar = () => {
-  const {activeMenu, setActiveMenu} = useStateContext();
+  const {activeMenu, setActiveMenu, screenSize} = useStateContext();
+  const handleCloseSidebar = () => {
+    if(activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  }
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
   const noramlLink =
@@ -20,8 +25,7 @@ const Sidebar = () => {
           <div className="flex justify-between items-center">
             <Link
               to="/"
-              onClick={() => {
-              }}
+              onClick={() => handleCloseSidebar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extralight tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware/> <span>Shoppy</span>
@@ -47,7 +51,7 @@ const Sidebar = () => {
                     <NavLink
                       to={`/${link.name}`}
                       key={link.name}
-                      onClick={() => setActiveMenu((prevState) => !prevState)}
+                      onClick={() => handleCloseSidebar}
                       className={({isActive}) =>
                         isActive ? activeLink : noramlLink
                       }
--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -12,11 +12,12 @@ const initialState = {
 export const ContextProvider = ({children}) => {
   const [activeMenu, setActiveMenu] = useState(true);
   const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState();
   const handleClick = (clicked) => {
     setIsClicked({ ...initialState, [clicked]: true})
   };
   return (
-    <StateContext.Provider value={{activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick}}>
+    <StateContext.Provider value={{activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize}}>
       {children}
     </StateContext.Provider>
   );
