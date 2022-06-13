fa986983b545486b4a695071475e44586321c16a
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-06-14 00:32:20 +0300

    Fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,9 +21,8 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
       <span
         style={{ background: dotColor }}
         className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
-      >
+      />
         {icon}
-      </span>
     </button>
   </TooltipComponent>
 );
@@ -46,7 +45,7 @@ const Navbar = () => {
       setActiveMenu(true);
     }
   }, []);
-  
+
   return (
     <div className="flex justify-between p-2 md:mx-6 relative">
       <NavButton
