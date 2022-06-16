cb08695498d6a322ec0cea3b012de6e7a287f206
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-06-17 00:09:18 +0300

    Revenue Section

--- src/components/Charts/SparkLine.js
+++ src/components/Charts/SparkLine.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import {
+  Inject,
+  SparklineTooltip,
+  SparklineComponent,
+} from "@syncfusion/ej2-react-charts";
 
-const SparkLine = () => {
+const SparkLine = ({ id, height, data, color, currentColor, width, type }) => {
   return (
-    <div>SparkLine</div>
-  )
-}
+    <SparklineComponent
+      id={id}
+      dataSource={data}
+      xName="x"
+      yName="yval"
+      type={type}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      tooltipSettings={{
+        visible: true,
+        // eslint-disable-next-line no-template-curly-in-string
+        format: "${x} : data ${yval}",
+        trackLineSettings: { visible: true },
+      }}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  );
+};
 
-export default SparkLine
+export default SparkLine;
--- src/components/Charts/Stacked.js
+++ src/components/Charts/Stacked.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import {
+  Category,
+  ChartComponent,
+  Inject,
+  Legend,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  StackingColumnSeries,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+import React from "react";
+import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from "../../data/dummy";
 
-const Stacked = () => {
+const Stacked = ({ width, height }) => {
   return (
-    <div>Stacked</div>
-  )
-}
+    <ChartComponent
+      width={width}
+      height={height}
+      id="stack chart"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{border: {width: 0}}}
+      tooltip={{ enable:true }}
+      LegendSettings={{ background: "white"}}
+    >
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
+};
 
-export default Stacked
+export default Stacked;
