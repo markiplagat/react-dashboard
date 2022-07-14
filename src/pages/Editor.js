1454386d9efe41992a763f179d5632745244815e
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-07-14 23:01:10 +0300

    Editor

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import {
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+  Toolbar,
+} from "@syncfusion/ej2-react-richtexteditor";
+
+import { Header } from "../components";
 
 const Editor = () => {
   return (
-    <div>Editor</div>
-  )
-}
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="Editor" category="App" />
+      <RichTextEditorComponent>
+        <Inject services={[Toolbar, Image, Link, QuickToolbar, HtmlEditor]} />
+      </RichTextEditorComponent>
+    </div>
+  );
+};
 
-export default Editor
+export default Editor;
